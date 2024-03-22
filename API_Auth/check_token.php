9501e6c9377c907d78e5e13c48c2f695bdc0b149
Mael <mael.goujon@etu.iut-tlse3.fr>
2024-03-22 12:25:59 +0100

    Mise à jour de la vérification du token dans check_token.php

--- API_Auth/check_token.php
+++ API_Auth/check_token.php
@@ -2,25 +2,34 @@
 
 require_once 'jwt_utils.php'; // Include the JWT utility functions
 
+
 $config = require_once 'config.php'; // Load the configuration
 
 $secret = $config['jwt_secret']; // Get the secret from the configuration
 
-function check_token() {
-    $headers = apache_request_headers(); // Get the request headers
+$headers = apache_request_headers(); // Get the request headers
+header('Content-Type: application/json');
+if (isset($headers['Authorization'])) { // Check if the Authorization header is set
+    $authorizationHeader = $headers['Authorization']; // Get the Authorization header
+    $headerValue = explode(' ', $authorizationHeader); // Split the header value
 
-    if (isset($headers['Authorization'])) { // Check if the Authorization header is set
-        $authorizationHeader = $headers['Authorization']; // Get the Authorization header
-        $headerValue = explode(' ', $authorizationHeader); // Split the header value
+    $token = $headerValue[1]; // Get the token from the header value
+    try {
 
-        $token = $headerValue[1]; // Get the token from the header value
-        try {
-            $isValid = is_jwt_valid($token, $GLOBALS['secret']); // Check if the token is valid
-            return $isValid;
-        } catch (Exception $e) {
-            return false;
+        $isValid = is_jwt_valid($token, $secret); // Check if the token is valid
+        if ($isValid) {
+            http_response_code(200);
+            echo json_encode(array("status" => "success", "status_code" => 200, "status_message" => "Token valide"));
+        } else {
+            http_response_code(401);
+            echo json_encode(array("status" => "error", "status_code" => 401, "status_message" => "Token invalide ou expiré"));
         }
-    } else {
-        return false;
+
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(array("status" => "error", "status_code" => 500, "status_message" => "Erreur dans le token"));
     }
+} else {
+    http_response_code(400);
+    echo json_encode(array("status" => "error", "status_code" => 400, "status_message" => "Token manquant"));
 }
